73e43026d9eddfccbff0a2213d404aa4e7db49fb
Benjamin Cornwell <33430452+bcornw2@users.noreply.github.com>
2020-01-30 09:47:43 -0500

    Update BlockBingScript.ps1

--- BlockBingScript.ps1
+++ BlockBingScript.ps1
@@ -1,4 +1,4 @@
-﻿# Script to implement Stop Bing Installation
+# Script to implement Stop Bing Installation
 #check for FSMO hold server, if this server is not FSMO hold, exit with error
 #check if GPMC pulls from central store.
 
@@ -27,21 +27,16 @@ if(!$CentralStore){
     Write-Host "Central Store created."
 }
 $FSMOserver = Get-ADDomainController -Filter * | Select-Object Name, Domain, Forest, OperationMasterRoles | Where-Object {$_.OperationMasterRoles} | Select-Object Name;
-$FSMOserver = $FSMOserver.DistinguishedName;
+$FSMOserver = $FSMOserver.Name;
 #Write-Host "FSMO server is $FSMOserver. You are on $env:COMPUTERNAME. Are you on the FSMO Server?  (Y/n)"
 if($env:COMPUTERNAME -ne $FSMOserver){
+    Write-Host "Do this on the other server. Exiting...";
     exit;
 }
-
-
-$ans = 'n'
-$ans = Read-Host -Prompt ">> "
-$ans.ToLower()
-
-if(($ans -eq 'y') -Or ($ans -eq 'yes') -Or ($ans -eq 'ye')){
+else{
+    Write-Host "`n You are on the correct server.";
     Write-Host "`n Continuing...";
 }
-else { Write-Host "Do this on the other server. Exiting..."; exit; }
 
 
 
@@ -50,19 +45,17 @@ New-Item -ItemType "Directory" -Path "C:\$env:HOMEPATH\Desktop\ExtractedADMX";
 #download .exe for new admx templates
 wget https://download.microsoft.com/download/2/E/E/2EEEC938-C014-419D-BB4B-D184871450F1/admintemplates_x64_4966-1000_en-us.exe -OutFile C:\$env:HOMEPATH\Desktop\admintemplates_x64_4966-1000_en-us.exe;
 
-$home = $env:HOMEPATH;
-
 #extract new templates to new directory
 Start-Process -FilePath "C:\$env:HOMEPATH\Desktop\admintemplates_x64_4966-1000_en-us.exe" -ArgumentList "/extract:C:\$env:HOMEPATH\Desktop\ExtractedADMX", "/quiet";
 
 #copy admx files to SYSVOL location
-Copy-Item -Path "C:\$env:HOMEPATH\Desktop\ExtractedADMX\admx\*.admx" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDOMAIN\Policies\PolicyDefinitions"
+Copy-Item -Path "C:\$env:HOMEPATH\Desktop\ExtractedADMX\admx\*.admx" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDOMAIN\Policies\PolicyDefinitions";
 
 #copy en_us file to SYSVOL location
 Copy-Item -Path "C:\$env:HOMEPATH\Desktop\ExtractedADMX\admx\en-us" -Destination "C:\Windows\SYSVOL\sysvol\$env:USERDNSDOMAIN\Policies\PolicyDefinitions" -Recurse -Force
 
 #Make the new GPO
-New-GPO -Name "Computer: Block Bing Search Extension" -Server $FSMOserver.Name
+New-GPO -Name "Computer: Block Bing Search Extension" -Server $FSMOserver;
 
 #Set GPO and assign it to the domain
 Set-GPRegistryValue -Name "Computer: Block Bing Search Extension" -Key "HKLM\SOFTWARE\Policies\Microsoft\office\16.0\common\officeupdate" -ValueName preventbinginstall -Type DWord -Value 1
@@ -71,6 +64,4 @@ Set-GPRegistryValue -Name "Computer: Block Bing Search Extension" -Key "HKLM\SOF
 $TARGET = Get-ADDomain | Select-Object DistinguishedName
 
 New-GPLink -Name "Computer: Block Bing Search Extension" -Target $TARGET.DistinguishedName -LinkEnabled Yes
-
-
 
